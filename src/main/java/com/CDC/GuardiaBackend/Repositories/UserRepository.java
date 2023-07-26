9459f919cb00f936a601872674fa620d08a01feb
Micaela Escanes <mica.escanes@gmail.com>
2023-07-26 12:54:00 -0300

    First Commit

---
+++ src/main/java/com/CDC/GuardiaBackend/Repositories/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.CDC.GuardiaBackend.Repositories;
+
+import com.CDC.GuardiaBackend.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author micae
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+    
+}
