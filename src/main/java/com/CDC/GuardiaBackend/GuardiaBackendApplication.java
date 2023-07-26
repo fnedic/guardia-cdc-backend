4b71e714c2f7a8356e6acf5939e0f28bdd3e0539
Micaela Escanes <mica.escanes@gmail.com>
2023-07-26 11:14:00 -0300

    CreateProyect

---
+++ src/main/java/com/CDC/GuardiaBackend/GuardiaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.CDC.GuardiaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardiaBackendApplication.class, args);
+                System.out.println("Hola desde el main");
+	}
+
+}
---
+++ src/test/java/com/CDC/GuardiaBackend/GuardiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CDC.GuardiaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
