d248507dbf424e066d87c7284a0db9d596f19e1a
Valentin Mathey <estudiovalentinmathey@gmail.com>
2023-07-28 14:34:40 -0300

    <feat> [Add values GroupProtocol]

--- src/main/java/com/CDC/GuardiaBackend/Enums/GroupProtocol.java
+++ src/main/java/com/CDC/GuardiaBackend/Enums/GroupProtocol.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package com.CDC.GuardiaBackend.Enums;
 
-/**
- *
- * @author mathe
- */
 public enum GroupProtocol {
-    
+    TRAUMATOLOGÍA,
+    URGENCIAS,
+    CARDIOVASCULAR,
+    RESPIRATORIO,
+    INFECTOLOGÍA,
+    UROLOGÍA,
+    CIRUGÍA,
+    TOXICOLOGÍA,
+    OTORRINOLARINGOLOGÍA,
+    GENERALES
 }
