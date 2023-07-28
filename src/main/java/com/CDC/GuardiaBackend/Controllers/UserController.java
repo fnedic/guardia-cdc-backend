d313835539b2f9b520723f4dc957d513d7f217b5
Valentin Mathey <estudiovalentinmathey@gmail.com>
2023-07-28 14:15:07 -0300

    <feat> [UserController]

--- src/main/java/com/CDC/GuardiaBackend/Controllers/UserController.java
+++ src/main/java/com/CDC/GuardiaBackend/Controllers/UserController.java
@@ -1,14 +1,42 @@
 package com.CDC.GuardiaBackend.Controllers;
 
+import com.CDC.GuardiaBackend.Entities.User;
+import com.CDC.GuardiaBackend.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.CDC.GuardiaBackend.Services.UserService;
+import java.util.List;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/cdc/")
 public class UserController {
     
     @Autowired
     UserService userService;
+    @Autowired
+    UserRepository userRepository;
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+    
+    
+    
+    @GetMapping("user")
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+    @PostMapping("user")
+    public User createUser(@RequestBody User user){
+         // Encriptar la contraseña antes de guardar al usuario
+//        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(encryptedPassword);
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/CDC/GuardiaBackend/Security/WebSecurity.java
+++ src/main/java/com/CDC/GuardiaBackend/Security/WebSecurity.java
@@ -3,11 +3,19 @@ package com.CDC.GuardiaBackend.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import com.CDC.GuardiaBackend.Services.UserService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurity {
-    
+
     @Autowired
-    public UserService userService; 
+    public UserService userService;
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
