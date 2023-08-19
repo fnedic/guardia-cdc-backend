ad6f42d6d5568f098d6a2f7c07e102e707fa9936
Facu Nedic <facunedic@gmail.com>
2023-08-19 13:51:44 -0300

    <Feat>: "ProtocolDashboard"

--- src/main/java/com/CDC/GuardiaBackend/Controllers/AdminController.java
+++ src/main/java/com/CDC/GuardiaBackend/Controllers/AdminController.java
@@ -2,16 +2,34 @@ package com.CDC.GuardiaBackend.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.CDC.GuardiaBackend.Repositories.UserRepository;
-import com.CDC.GuardiaBackend.Services.UserService;
 
+import com.CDC.GuardiaBackend.Repositories.ProtocolRepository;
+import com.CDC.GuardiaBackend.Repositories.UserRepository;
+import com.CDC.GuardiaBackend.Services.ProtocolService;
+import com.CDC.GuardiaBackend.Services.UserService;
+import java.util.List;
+import com.CDC.GuardiaBackend.Entities.Protocol;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/admin/")
 public class AdminController {
     
     @Autowired
     UserService userService;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    ProtocolService protocolService;
+    @Autowired
+    ProtocolRepository protocolRepository;
+
+    @GetMapping("protocol")
+    public List<Protocol> getProtocol() {
+        return protocolRepository.findAll();
+    }
 }
